1c2f29a4b2a0bf8cad63e60934b1955e5435c4c6
mohyangba <mohyangba@users.noreply.github.com>
2023-11-09 15:36:57 +0900

    04

--- main.c
+++ main.c
@@ -4,14 +4,12 @@
 
 int main(int argc, char *argv[])
 {
-  char src[] = "The worst thingss to eat before you sleep";
-  char dst[100];
-  
-  // 복사 대상 문자열 = strcpy(대상, 원본); 
-  
-  strcpy(dst, src);
-  printf("copied string: %s\n", dst); 
+  char src[30] = "happy happy happy"; // 30 없어도 가능  
+
+  printf("sring \"%s\" lenght: %i\n", src, strlen(src)); // "를 프린트 하고 싶다면 \"사용  
  
+ // 문자열 길이 = strlen(입력문자열); 
+ // src[30]이라고 해도 배열의 크기 가 아닌 문자 의 수 를 세준다 
   system("PAUSE");	
   return 0;
 }
