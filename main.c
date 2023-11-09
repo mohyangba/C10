8c25e5eaed422c09f95194ab0e288d4ac49b8e1d
mohyangba <mohyangba@users.noreply.github.com>
2023-11-09 16:10:44 +0900

    06

--- main.c
+++ main.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-  int i=0;
-  char str[4];
-  
-  str[0] = 'a';
-  str[1] = 'b';
-  str[2] = 'c';
-  str[3] = '\0';
-  
-  printf("%s\n", str); 
-  
-  while ( str[i] != '\0' ) { //현재 문자 str[i]이 맨 끝이 아님을 while문의 조건으로 입력 
-          printf("%c", str[i]); // 현재 문자 출력 
-          i++; //i를 1 증가 
-  }
-  
+    FILE* fp;
+    char c; //한 글자씩 읽어 올 때 쓸 변수 설정  
+    // 1. open file 
+    fp = fopen("sample.txt", "r"); 
+    //fopen("파일이름", "r") 파일을 읽을 것이라는 뜻  
+    //안전 장치 설정
+    if (fp == NULL)
+    {
+           printf("failed to open\n");
+           return 0;
+    } 
+   
+   //2. read file
+   while( (c=fgetc(fp)) != EOF ) // 한 글자 읽은 게 마지막이 아니면 
+   {
+          putchar(c);// 한 글자 출력
+   }
+           
+   //3. close file
+    fp = fclose(fp);
+    
   system("PAUSE");	
   return 0;
 }
