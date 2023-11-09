2d6f83194dd23ab51499626016e26c1d23ba4d6c
mohyangba <mohyangba@users.noreply.github.com>
2023-11-09 16:06:45 +0900

    02

--- main.c
+++ main.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main(int argc, char *argv[])
 {
-  FILE* fp;
-  char str[100]; // 얼마 들어올지 모르니까 대충 큰 숫자
-  int i = 0;
-   // 1. open file 
-   fp = fopen("sample.txt", "w"); //fopen("파일이름", "w") 파일에 쓰기를 할 것이라는 뜻  
-   
-   //2. write file
-
-   for(i=0; i<3; i++)
-   {
-        
-   //2-1) print " input a word "
-   printf("input a word: ");
-   
-   //2-2) scanf a string
-   scanf("%s", &str);
-   
-   //2-3) fprintf()
-   fprintf(fp, "%s\n", str); // 앞에 포인터로 어느 파일을 열지 알려주기  
-   }
-   
-   //3. close file
-    fp = fclose(fp);
-    
+  int i=0;
+  char str[4];
+  
+  str[0] = 'a';
+  str[1] = 'b';
+  str[2] = 'c';
+  str[3] = '\0';
+  
+  printf("%s\n", str); 
+  
+  while ( str[i] != '\0' ) { //현재 문자 str[i]이 맨 끝이 아님을 while문의 조건으로 입력 
+          printf("%c", str[i]); // 현재 문자 출력 
+          i++; //i를 1 증가 
+  }
+  
   system("PAUSE");	
   return 0;
 }
