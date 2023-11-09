23e8fba9d67772490b68469cbcc1f54dbb7f5044
mohyangba <mohyangba@users.noreply.github.com>
2023-11-09 15:58:07 +0900

    05

--- main.c
+++ main.c
@@ -4,12 +4,30 @@
 
 int main(int argc, char *argv[])
 {
-  char src[30] = "happy happy happy"; // 30 없어도 가능  
+  FILE* fp;
+  char str[100]; // 얼마 들어올지 모르니까 대충 큰 숫자
+  int i = 0;
+   // 1. open file 
+   fp = fopen("sample.txt", "w"); //fopen("파일이름", "w") 파일에 쓰기를 할 것이라는 뜻  
+   
+   //2. write file
 
-  printf("sring \"%s\" lenght: %i\n", src, strlen(src)); // "를 프린트 하고 싶다면 \"사용  
- 
- // 문자열 길이 = strlen(입력문자열); 
- // src[30]이라고 해도 배열의 크기 가 아닌 문자 의 수 를 세준다 
+   for(i=0; i<3; i++)
+   {
+        
+   //2-1) print " input a word "
+   printf("input a word: ");
+   
+   //2-2) scanf a string
+   scanf("%s", &str);
+   
+   //2-3) fprintf()
+   fprintf(fp, "%s\n", str); // 앞에 포인터로 어느 파일을 열지 알려주기  
+   }
+   
+   //3. close file
+    fp = fclose(fp);
+    
   system("PAUSE");	
   return 0;
 }
